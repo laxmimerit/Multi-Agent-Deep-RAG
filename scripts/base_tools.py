aee81d29baf74e252a4a27d9bc1ca584d1919e9a
laxmimerit <laxmimerit@gmail.com>
2025-12-07 18:28:45 +0530

    Gemini 3 with langchain bootcamp

---
+++ scripts/base_tools.py
@@ -0,0 +1,59 @@
+import os
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.tools import tool
+import ollama
+import requests
+
+
+# -------------------------
+# Web Search Tool
+# -------------------------
+
+@tool
+def web_search(query: str) -> str:
+    """
+    Perform a live web search using Ollama Cloud Web Search API.
+
+    Input:
+        query: search query string
+
+    Output:
+        JSON string of top results (max_results=2).
+    """
+    response = ollama.web_search(query, max_results=2)
+    response = response.results
+
+    return response
+
+
+# -------------------------
+# Weather Tool
+# -------------------------
+@tool
+def get_weather(location: str) -> str:
+    """Get current weather for a location using WeatherAPI.com.
+    
+    Use for queries about weather, temperature, or conditions in any city.
+    Examples: "weather in Paris", "temperature in Tokyo", "is it raining in London"
+    
+    Args:
+        location: City name (e.g., "New York", "London", "Tokyo")
+        
+    Returns:
+        Current weather information including temperature and conditions.
+    """
+
+    url = (
+        "http://api.weatherapi.com/v1/current.json"
+        f"?key={os.getenv("WEATHER_API_KEY")}&q={location}&aqi=no"
+    )
+
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+    return data
